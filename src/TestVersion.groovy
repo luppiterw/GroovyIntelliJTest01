b2dd1eedd06cf677022f097f9e1ab90599630bae
hughie <530125717@qq.com>
2016-10-25 09:40:49 +0800

    添加一个用于git版本测试的groovy代码

---
+++ src/TestVersion.groovy
@@ -0,0 +1,29 @@
+/**
+ * Created by hughie on 2016/10/25.
+ */
+
+def myVersionName = "i5cnc"
+def getMyVersionName = {
+    -> "hhh"
+}
+def getMyVersionCode = {
+    it ->
+        Process process = Runtime.getRuntime().exec("git branch -v")// | grep develop")
+        String trimProcessStr = process.getText().trim()
+        trimProcessStr = trimProcessStr.substring(trimProcessStr.indexOf("$it") + "$it".length()).trim()
+        println("$it")
+        trimProcessStr = trimProcessStr.substring(0, trimProcessStr.indexOf(' ')).trim()
+        println(trimProcessStr)
+        long val = Long.parseUnsignedLong(trimProcessStr, 16)//"0x$trimProcessStr".toLong()
+        val
+        println(val)
+//        Long.toHexString(val)
+        Integer.parseInt(trimProcessStr, 16)
+//        Integer.MAX_VALUE
+}
+
+println(myVersionName)
+println(getMyVersionName())
+//println(getMyVersionCode("develop"))
+println(getMyVersionCode("master"))
+//println((char)getMyVersionCode("master"))
